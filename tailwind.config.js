9289633f72a8661ab3f69bd70553da9b37ae53e9
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-11-11 13:03:45 +0100

    Changed Favicon and Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,37 @@
-import { config } from './lib/server/config'
-import { FONTS_SANS, FONTS_SERIF } from './consts'
+import { config } from "./lib/server/config";
+import { FONTS_SANS, FONTS_SERIF } from "./consts";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
-  darkMode: 'class',
+  content: ["./pages/**/*.js", "./components/**/*.js", "./layouts/**/*.js"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         day: {
-          DEFAULT: config.lightBackground || '#ffffff'
+          DEFAULT: config.lightBackground || "#ffffff",
         },
         night: {
-          DEFAULT: config.darkBackground || '#111827'
-        }
+          DEFAULT: config.darkBackground || "#111827",
+        },
       },
       fontFamily: {
         sans: FONTS_SANS,
         serif: FONTS_SERIF,
         noEmoji: [
+          '"Overused Grotesk"',
           '"IBM Plex Sans"',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'sans-serif'
-        ]
-      }
-    }
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "sans-serif",
+        ],
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
