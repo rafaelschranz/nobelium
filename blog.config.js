fcbf6a13183f010512bb0107fafc539a3aaa2e63
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-11-11 13:25:51 +0100

    Changed the footer and blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
-  socialLink: "https://twitter.com/craigaryhart",
+  socialLink: "https://www.linkedin.com/in/rafael-schranz/",
   seo: {
     keywords: ["Blog", "Website", "Rafael's Blog", "Rafael's Brain"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
