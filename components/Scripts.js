cd9f7c5c085708778baf0c55d4ec788b93d2310b
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-11-11 13:58:33 +0100

    added umami analytics

--- components/Scripts.js
+++ components/Scripts.js
@@ -1,19 +1,19 @@
-import Script from 'next/script'
-import { useConfig } from '@/lib/config'
+import Script from "next/script";
+import { useConfig } from "@/lib/config";
 
 const Scripts = () => {
-  const BLOG = useConfig()
+  const BLOG = useConfig();
 
   return (
     <>
-      {BLOG.analytics && BLOG.analytics.provider === 'ackee' && (
+      {BLOG.analytics && BLOG.analytics.provider === "ackee" && (
         <Script
           src={BLOG.analytics.ackeeConfig.tracker}
           data-ackee-server={BLOG.analytics.ackeeConfig.dataAckeeServer}
           data-ackee-domain-id={BLOG.analytics.ackeeConfig.domainId}
         />
       )}
-      {BLOG.analytics && BLOG.analytics.provider === 'ga' && (
+      {BLOG.analytics && BLOG.analytics.provider === "ga" && (
         <>
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.analytics.gaConfig.measurementId}`}
@@ -28,8 +28,15 @@ const Scripts = () => {
           </Script>
         </>
       )}
+      {BLOG.analytics && BLOG.analytics.provider === "umami" && (
+        <script
+          defer
+          src="https://analytics.rafaelschranz.com/script.js"
+          data-website-id="b34463b9-5f13-4a1f-9be1-271b8dbdd97d"
+        ></script>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Scripts
+export default Scripts;
