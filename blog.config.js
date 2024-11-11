82325c8eab2ee7e0331f193347aa6ef0bec2b8ff
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-11-11 11:17:56 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Rafael Schranz',
   email: 'hi@rafaelschranz.com',
   link: 'https://blog.rafaelschranz.com',
-  description: 'Rafael's Inner Circuit',
+  description: "Rafael's Inner Circuit",
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Europe/Zurich', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
